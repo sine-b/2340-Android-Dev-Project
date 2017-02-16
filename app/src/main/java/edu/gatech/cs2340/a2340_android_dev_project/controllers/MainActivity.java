3f9cf9276b025cc1e45883eb1375921f1f31005c
Nate <natehazelnut@gmail.com>
2017-02-15 21:11:27 -0500

    added a button to log out from the main screen

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cs2340.a2340_android_dev_project.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignOutButtonClicked();
+            }
+        });
+    }
+
+    public void onSignOutButtonClicked() {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
     }
 }

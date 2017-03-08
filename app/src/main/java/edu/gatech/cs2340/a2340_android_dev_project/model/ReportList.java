cd976c8a77cc80ee7ec8a02a5570b009bad963fe
Nate <natehazelnut@gmail.com>
2017-03-08 10:44:17 -0500

    Javadoced getters and setters (apparently we have to do those too)

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/model/ReportList.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/model/ReportList.java
@@ -7,12 +7,24 @@ public class ReportList {
     ArrayList<Report> reportList = new ArrayList<>();
     int idCounter = 0;
 
+    /**
+     * Adds a new report to the ReportList with a
+     * system-assigned id
+     *
+     * @param report the Report to be added
+     */
     public void addReport(Report report) {
         report.setId(idCounter);
         reportList.add(report);
         idCounter++;
     }
 
+    /**
+     * Returns an array of the Reports in the
+     * ReportList. For use with ArrayAdapters.
+     *
+     * @return array of ReportList's reports
+     */
     public Report[] getReports() {
         Report[] reportArray = new Report[reportList.size()];
         for (int i = 0; i < reportArray.length; i++) {

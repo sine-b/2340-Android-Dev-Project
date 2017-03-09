48894f8a756cf133ed55729e7d309a63163731ae
Nate <natehazelnut@gmail.com>
2017-03-09 00:28:33 -0500

    Map now populates with nearby reports

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/MapsActivity.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/MapsActivity.java
@@ -8,7 +8,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.gatech.cs2340.a2340_android_dev_project.model.Report;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -18,6 +21,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -38,9 +42,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // populate map with nearby water reports
+        for (Report report : MainActivity.reportList.getReports()) {
+            LatLng latLng = new LatLng(report.getLatitude(), report.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Report #" + report.getId());
+            mMap.addMarker(markerOptions);
+        }
     }
 }

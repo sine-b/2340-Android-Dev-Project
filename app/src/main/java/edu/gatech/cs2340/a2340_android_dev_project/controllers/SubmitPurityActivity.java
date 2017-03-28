caa894f7ac1850ca017fedca000d5c69df1bbc22
tthole3 <tthole3@gatech.edu>
2017-03-27 22:42:44 -0400

    updates to purity stuff

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/SubmitPurityActivity.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/SubmitPurityActivity.java
@@ -1,10 +1,19 @@
 package edu.gatech.cs2340.a2340_android_dev_project.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import edu.gatech.cs2340.a2340_android_dev_project.R;
+import com.google.android.gms.maps.model.LatLng;
 
+import edu.gatech.cs2340.a2340_android_dev_project.model.Report;
+import edu.gatech.cs2340.a2340_android_dev_project.model.WaterCondition;
+import edu.gatech.cs2340.a2340_android_dev_project.model.WaterType;
 public class SubmitPurityActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +21,11 @@ public class SubmitPurityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_purity);
 
-
+        // set up type spinner
+        conditionSpinner = (Spinner) findViewById(R.id.typeSpinner);
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, WaterType.values());
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        conditionSpinner.setAdapter(typeAdapter);
+        conditionSpinner.setSelection(ConditionType.SAFE.ordinal());
     }
 }

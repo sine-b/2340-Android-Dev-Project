7b774aa5f6495063fbae68806fb199ef274c8ceb
tthole3 <tthole3@gatech.edu>
2017-03-28 01:52:06 -0400

    added purityreportlist

---
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/ListPurityActivity.java
@@ -0,0 +1,47 @@
+package edu.gatech.cs2340.a2340_android_dev_project.controllers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListPurityActivity extends AppCompatActivity {
+    private ListView reportListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_purity);
+
+        // set up list view
+        reportListView = (ListView) findViewById(R.id.reportListView);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, MainActivity.purityReportList.getReportList());
+        reportListView.setAdapter(listAdapter);
+
+        // event handler for report on list
+        reportListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                ReportDetailsActivity.setReport(MainActivity.purityReportList.getReportList().get(position));
+                onReportPressed(view);
+
+            }
+        });
+    }
+
+    /**
+     * Function for each ListView item's onClick method.
+     * Sends user to ReportDetailsActivity where detailed
+     * information about the report they tapped is displayed.
+     *
+     * @param v the view the onItemClickListener belongs to
+     */
+    public void onReportPressed(View v) {
+        Intent intent = new Intent(this, ReportDetailsActivity.class);
+        startActivity(intent);
+    }
+}

d73736d998c44cc2d2947fbc08776a008cd756ff
tthole3 <tthole3@gatech.edu>
2017-03-27 22:55:24 -0400

    updates to purity stuff

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/SubmitPurityActivity.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/SubmitPurityActivity.java
@@ -12,20 +12,29 @@ import android.widget.Toast;
 import com.google.android.gms.maps.model.LatLng;
 
 import edu.gatech.cs2340.a2340_android_dev_project.model.Report;
-import edu.gatech.cs2340.a2340_android_dev_project.model.WaterCondition;
-import edu.gatech.cs2340.a2340_android_dev_project.model.WaterType;
+import edu.gatech.cs2340.a2340_android_dev_project.model.ConditionType;
 public class SubmitPurityActivity extends AppCompatActivity {
+    private Spinner conditionSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_purity);
 
-        // set up type spinner
+        // set up condition spinner
         conditionSpinner = (Spinner) findViewById(R.id.typeSpinner);
-        ArrayAdapter<String> typeAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, WaterType.values());
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, ConditionType.values());
         typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         conditionSpinner.setAdapter(typeAdapter);
         conditionSpinner.setSelection(ConditionType.SAFE.ordinal());
+
+        // event handler for the select location button
+        Button selectLocationButton = (Button) findViewById(R.id.locationSelecterButton);
+        selectLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSelectLocationButtonPressed(view);
+            }
+        });
     }
 }
--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/model/ConditionType.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/model/ConditionType.java
@@ -1,8 +1,19 @@
 package edu.gatech.cs2340.a2340_android_dev_project.model;
 
 /**
- * Created by Thomas on 3/27/2017.
+ * Enumeration that defines water conditions for purity reports
  */
-
 public enum ConditionType {
+    SAFE("SAFE"), TREATABLE("TREATABLE"), UNSAFE("UNSAFE");
+
+    private String rep;
+
+    ConditionType(String rep) {
+        this.rep = rep;
+    }
+
+    @Override
+    public String toString() {
+        return rep;
+    }
 }

fcbd7c0a8d32dc976dc07a0522f5f1869d91386e
sdang3 <sdang3@gatech.edu>
2017-03-28 01:40:16 -0400

    Created list choice activity and view

---
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/ListChoiceActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.a2340_android_dev_project.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.a2340_android_dev_project.R;
+
+public class ListChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_choice);
+    }
+}

0c988354661b33aceaa731ef0548c67799fdeed4
sdang3 <sdang3@gatech.edu>
2017-04-24 22:13:53 -0400

    added method for facebook activity result

--- app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/a2340_android_dev_project/controllers/LoginActivity.java
@@ -84,6 +84,12 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
     /**
      * Function for the sign in button's onClick method. Validates
      * the login credentials (which have been hardcoded here for now)
